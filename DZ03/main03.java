6bacefb907a7c95017c312f8b6335a42cc4610c4
Emil <Usikov.emil@yandex.ru>
2023-06-20 17:07:17 +0500

    Задание 3 Готово

--- DZ03/main03.java
+++ DZ03/main03.java
@@ -10,7 +10,12 @@ public class main03 {
         for (int i = 0; i < 100; i++) {
             numbers.add(rand.nextInt(100));
         }
-
-        System.out.println(numbers);
+        numbers.removeIf(n -> n % 2 == 0); //удаляем из списка чётные числа
+        int min = numbers.stream().min(Integer::compare).get(); // находим минимальное значение
+        int max = numbers.stream().max(Integer::compare).get(); // находим максимальное значение
+        double avg = numbers.stream().mapToInt(Integer::intValue).average().getAsDouble(); // находим среднее значение
+        System.out.println("Минимальное значение: " + min);
+        System.out.println("Максимальное значение: " + max);
+        System.out.println("Среднее значение: " + avg);
     }
 }
