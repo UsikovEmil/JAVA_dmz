a4dc861110c85c33f2d78cdb862ebb490c2f0a82
Usikov Emil <usikov.emil@yandex.ru>
2023-06-30 21:09:30 +0500

    Задание 4 готово

--- DZ04/main04.java
+++ DZ04/main04.java
@@ -1,5 +1,51 @@
 package DZ04;
+import java.util.*;
+
+class User {
+    String fName;
+    String lName;
+    String mName;
+    int age;
+    String pol;
+
+    public User(String fName, String lName, String mName, int age, String pol) {
+        this.fName = fName;
+        this.lName = lName;
+        this.mName = mName;
+        this.age = age;
+        this.pol = pol;
+    }
+
+    public String toString() {
+        return lName + " " + fName.charAt(0) + "." + mName.charAt(0) + ". " + age + " " + pol;
+    }
+}
 
 public class main04 {
-    
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<User> users = new ArrayList<>();
+
+        while (true) {
+            System.out.println("Введите информацию о пользователе (или 'выход' для выхода из программы):");
+            String input = scanner.nextLine();
+            if (input.equals("выход")) {
+                break;
+            }
+            String[] parts = input.split(" ");
+            String fName = parts[0];
+            String lName = parts[1];
+            String mName = parts[2];
+            int age = Integer.parseInt(parts[3]);
+            String pol = parts[4];
+            users.add(new User(fName, lName, mName, age, pol));
+        }
+
+        System.out.println("Список пользователей (отсортированный по возрасту):");
+        users.sort(Comparator.comparingInt(user -> user.age));
+        for (User user : users) {
+            System.out.println(user);
+        }
+    }
 }
